8eefb20a600681b1d5f24521626b3b305059b8e9
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-08-17 22:38:27 +0900

    Add: スペルミス修正

--- laravel/app/Http/Requests/ArticleRequest.php
+++ laravel/app/Http/Requests/ArticleRequest.php
@@ -24,8 +24,16 @@ class ArticleRequest extends FormRequest
   public function rules()
   {
     return [
-      'title' => 'requiredlmax:50',
-      'body' => 'requiredlmax:500',
+      'title' => 'required|max:50',
+      'body' => 'required|max:500',
+    ];
+  }
+
+  public function attributes()
+  {
+    return [
+      'title' => 'タイトル',
+      'body' => '本文',
     ];
   }
 }
