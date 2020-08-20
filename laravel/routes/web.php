f95a304b07b90a2d8599678d35cbb5807913bd6c
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-08-20 21:40:12 +0900

    Fix: ルーティング変更

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -4,3 +4,7 @@ Auth::routes();
 Route::get('/', 'ArticleController@index')->name('articles.index');
 Route::resource('/articles', 'ArticleController')->except(['index'])->middleware('auth');
 Route::resource('/articles', 'ArticleController')->only(['show']); // この書き方でshowではauthミドルウェアを使わないよう設定できる
+Route::prefix('article')->name('articles')->group(function() {
+  Route::put('/{article}/like', 'ArticleController@like')->name('like')->middleware('auth');
+  Route::delete('/{article}/like', 'ArticleController@unlike')->name('unlike')->middleware('auth')
+});
