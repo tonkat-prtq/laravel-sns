ac33469dbefd9ccc180ff50bd0e0f69165d76f01
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-08-22 17:00:44 +0900

    Bug: Googleログインの画面が表示されない

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 Auth::routes();
+Route::prefix('login')->name('login.')->group(function() {
+  Route::get('/{provider}', 'Auth\Login\Controller@redirectToProvider')->name('{provider}');
+});
 Route::get('/', 'ArticleController@index')->name('articles.index');
 Route::resource('/articles', 'ArticleController')->except(['index', 'show'])->middleware('auth');
 Route::resource('/articles', 'ArticleController')->only(['show']);
