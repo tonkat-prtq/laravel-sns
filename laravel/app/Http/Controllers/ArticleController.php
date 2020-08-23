787efa460f1731f9fda8fbc66d2e6d2568de4226
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-08-23 22:16:45 +0900

    Add: tag, likesもまとめてロード

--- laravel/app/Http/Controllers/ArticleController.php
+++ laravel/app/Http/Controllers/ArticleController.php
@@ -21,8 +21,8 @@ class ArticleController extends Controller
   public function index()
   {  
     $articles = Article::all()->sortByDesc('created_at')
-      ->load('user');
-      
+      ->load(['user', 'likes', 'tags']);
+
     return view('articles.index', ['articles' => $articles]);
   }
 
